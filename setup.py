6fba81a13adfc050b29eb52eb7de2d2d5927da64
Jeroen van der Heijden <jvdheijden@insign.it>
2016-10-03 20:25:58 +0200

    Update changelog

--- setup.py
+++ setup.py
@@ -1,31 +1,18 @@
-#
-#  Got documentation from: http://peterdowns.com/posts/first-time-with-pypi.html
-#
-#   1. Create tag:
-#       git tag 2.0.0 -m "Adds a tag so that we can put this new version on PyPI."
-#
-#   2. Push tag:
-#       git push --tags origin master
-#
-#   3. Upload your package to PyPI Test:
-#       python setup.py register -r pypitest
-#       python setup.py sdist upload -r pypitest
-#
-#   4. Upload to PyPI Live
-#       python setup.py register -r pypi
-#       python setup.py sdist upload -r pypi
-#
-
 from distutils.core import setup
+
+VERSION = '2.0.0'
+
 setup(
     name='siridb-connector',
     packages=['siridb-connector'],
-    version='2.0.0',
+    version=VERSION,
     description='SiriDB Connector',
     author='Jeroen van der Heijden',
     author_email='jeroen@transceptor.technology',
     url='https://github.com/transceptor-technology/siridb-connector',
-    download_url='https://github.com/transceptor-technology/siridb-connector/tarball/2.0.0',
+    download_url=
+        'https://github.com/transceptor-technology/'
+        'siridb-connector/tarball/{}'.format(VERSION),
     keywords=['siridb', 'connector', 'database', 'client'],
     classifiers=[
         'Development Status :: 4 - Beta',
---
+++ siridb/connector/lib/__init__.py
@@ -0,0 +1,5 @@
+'''SiriDB Connector - Communication with SiriDB
+
+:copyright: 2016, Jeroen van der Heijden (Transceptor Technology)
+:license: MIT
+'''
