545c61e6b17c8eec4066e94bb068539a491f45d0
Jeroen van der Heijden <jeroen@transceptor.technology>
2021-03-15 15:35:59 +0100

    Upd setup.py

--- setup.py
+++ setup.py
@@ -45,6 +45,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Database',
         'Topic :: Software Development'
